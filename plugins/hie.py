7b80517125aa7396b34fad2d41d15f366e91ee9b
bhoomi-s-007 <71869549+bhoomi-s-007@users.noreply.github.com>
2021-03-26 15:48:04 +0530

    Create hie.py

---
+++ plugins/hie.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2020-2021 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/UsergeTeam/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from datetime import datetime
+
+from userge import userge, Message, get_version
+
+
+@userge.on_cmd("hie", about={
+    'header': "check pingtime, uptime and version of your userbot",
+    'flags': {'-a': "average ping"}}, group=-1)
+async def heyme(message: Message):
+    start = datetime.now()
+    await message.edit('`Hillo!`')
+    end = datetime.now()
+    m_s = (end - start).microseconds / 1000
+    up_time = userge.uptime
+    ub_version = get_version()
+    await message.edit(f"**Hillo!**\nPing :` {m_s} m`\nUptime :` {up_time}`\nVersion :` {ub_version}`")
