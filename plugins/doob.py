5300225e9a12bd00d3933f1411441d5f4cca7036
bhoomi-s-007 <71869549+bhoomi-s-007@users.noreply.github.com>
2021-03-26 16:55:58 +0530

    Create doob.py

---
+++ plugins/doob.py
@@ -0,0 +1,33 @@
+import asyncio
+from random import choice
+from userge import userge, Message
+
+
+async def check_and_send(message: Message, *args, **kwargs):
+    replied = message.reply_to_message
+    if replied:
+        await asyncio.gather(
+            message.delete(),
+            replied.reply(*args, **kwargs)
+        )
+    else:
+        await message.edit(*args, **kwargs)
+
+
+@userge.on_cmd("doob$", about={'header': "A special plugin for our\nasim_ansarii\n😂😂😂"})
+
+async def doob_func(message):
+	gali = choice(doob)
+	stickers = choice(stickers_ids)
+	await check_and_send(message, "<b>{}</b>".format(doob), parse_mode='html')
+	await message.reply_sticker(sticker="".join(stickers))
+
+
+
+doob = (
+    "@Wasim_ansarii Doob Maro🥺🥺",
+    "@Wasim_ansarii Ab to doob maro please🥺🥺")
+stickers_ids = (
+    "",
+    "CAACAgEAAx0CVKYprwACRV1gXcNqmPFQwNVcwhxHllfzTEX3JQACOgEAAlSR4EWCGjrKsMOrbB4E",
+    "CAACAgEAAx0CVKYprwACRWBgXcOglSa_nlSXP9Tuco_KAAHSmPAAApUAA-Au2EcG3MYpfKPRRh4E")
